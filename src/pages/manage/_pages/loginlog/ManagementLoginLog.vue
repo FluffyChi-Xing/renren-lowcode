8772d992f571159cc7ac607e26e28df145304c17
FluffyChi-Xing <121754413+FluffyChi-Xing@users.noreply.github.com>
2025-05-08 18:29:59 +0800

    登录日志页面暂提

--- src/pages/manage/_pages/loginlog/ManagementLoginLog.vue
+++ src/pages/manage/_pages/loginlog/ManagementLoginLog.vue
@@ -1,11 +1,117 @@
 <script setup lang="ts">
+import { ref, reactive } from "vue";
+import ManageLayout from "@/pages/manage/_component/ManageLayout.vue";
 
+
+
+/** ========== 登录日志初始化-start ==========**/
+const searchValue = ref<string>('');
+const isShow = ref<boolean>(true);
+const isLoading = ref<boolean>(false);
+/** ========== 登录日志初始化-end ==========**/
 </script>
 
 <template>
-  <div class="w-full h-full flex flex-col bg-red-500 items-center justify-center text-white">
-    登录日志
-  </div>
+  <ManageLayout
+    header="登录日志"
+  >
+    <template #default>
+      <div class="w-full h-full flex pt-4 flex-col">
+        <!-- search-area -->
+        <div
+          v-if="isShow"
+          class="w-full h-auto flex items-center justify-between"
+        >
+          <el-form-item label="查询" required inline-message>
+            <el-input
+              v-model="searchValue"
+              clearable
+              placeholder="请输入查询条件"
+              style="width: 240px;"
+            />
+          </el-form-item>
+          <el-button type="primary">查询</el-button>
+        </div>
+        <!-- functional-banner -->
+        <div class="w-full h-auto flex items-center justify-between">
+          <el-button type="success" plain icon="Upload">导出</el-button>
+          <div>
+            <el-button
+              @click="() => isShow = !isShow"
+              type="primary"
+              circle
+              size="small"
+              icon="Search"
+              plain
+            />
+            <el-button
+              icon="Refresh"
+              type="info"
+              circle
+              plain
+              size="small"
+            />
+            <el-button
+              plain
+              type="warning"
+              size="small"
+              icon="Operation"
+              circle
+            />
+          </div>
+        </div>
+        <!-- table-data -->
+        <div class="w-full h-auto flex mt-4 flex-col">
+          <el-table
+            v-loading="isLoading"
+            stripe
+            border
+            fit
+            :header-cell-style="{ backgroundColor: '#33FF33', alignItems: 'center', color: '#000' }"
+            class="h-full"
+            style="width: 100%;"
+          >
+            <el-table-column
+              label="id"
+              prop="id"
+            />
+            <el-table-column
+              label="用户id"
+              prop="userId"
+            />
+            <el-table-column
+              label="用户名"
+              prop="username"
+            />
+            <el-table-column
+              label="ip"
+              prop="ip"
+            />
+            <el-table-column
+              label="浏览器"
+              prop="browser"
+            />
+            <el-table-column
+              label="登录时间"
+              prop="createTime"
+            />
+            <el-table-column
+              label="状态"
+              prop="status"
+            />
+            <template #empty>
+              <el-empty
+                description="暂无登录日志"
+              />
+            </template>
+          </el-table>
+        </div>
+      </div>
+    </template>
+    <template #footer>
+      <div class="w-full h-full flex items-center bg-red-500" />
+    </template>
+  </ManageLayout>
 </template>
 
 <style scoped>
